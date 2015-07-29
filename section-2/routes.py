5a68582c438bfbd8721d70780b8da2cb0fccd286
Lalith Polepeddi <lpolepeddi@gmail.com>
2015-07-29 15:11:12 -0600

    Add section 2

---
+++ section-2/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
